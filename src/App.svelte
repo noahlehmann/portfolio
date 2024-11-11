fc186c673ce28178198a2f87292bbb7351ee87cd
Noah Lehmann <noah.lehmann@protonmail.com>
2024-11-11 23:10:26 +0100

    main: Add github actions badge

--- src/App.svelte
+++ src/App.svelte
@@ -6,4 +6,9 @@
 <main>
   <Header />
   <p>{$_('description')}</p>
+  <p>See how its deployed:</p>
+  <p>Intellij -> Github -> Actions -> Build -> Upload -> Serve -> Domain</p>
+  <a href="https://github.com/noahlehmann/portfolio/actions/workflows/static.yml">
+    <img alt="Deploy status badge"  src="https://github.com/noahlehmann/portfolio/actions/workflows/static.yml/badge.svg">
+  </a>
 </main>
