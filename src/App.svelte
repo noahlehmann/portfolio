991de4a2d3b0f21e8569e9f9b42248223991b8f6
Noah Lehmann <noah.lehmann@protonmail.com>
2024-11-11 22:21:23 +0100

    main: Add first component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script>
   import { _ } from "svelte-i18n";
+  import Header from "./components/header/Header.svelte";
 </script>
 
 <main>
-  <h1>{$_('welcome')}</h1>
+  <Header />
   <p>{$_('description')}</p>
 </main>
---
+++ src/components/header/Header.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { _ } from "svelte-i18n";
+</script>
+
+<div class="header">
+    <h1>{$_('welcome')}</h1>
+</div>
