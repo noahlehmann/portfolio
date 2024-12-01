b51ae9d9b42d923962ea08f418900088eb45b001
Noah Lehmann <noah.lehmann@protonmail.com>
2024-12-01 22:44:30 +0100

    main: Add footer

---
+++ src/components/footer/Footer.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { _ } from "svelte-i18n";
+    import { faGithub } from '@fortawesome/free-brands-svg-icons';
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+
+    let year = new Date().getFullYear();
+</script>
+
+<div class="footer">
+    <a href="https://github.com/noahlehmann" class="icon-link" target="_blank">
+        <FontAwesomeIcon icon={faGithub} size="2x"/>
+    </a>
+    <p>{$_('footer.rights')} {year} - Noah Lehmann</p>
+</div>
+
+<style>
+    .footer {
+        padding: 2rem 4rem 3rem 4rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .icon-link:visited {
+        color: var(--color);
+    }
+</style>
--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -5,3 +5,12 @@
 <div class="header">
     <h1>{$_('welcome')}</h1>
 </div>
+
+<style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding: 2rem 4rem 2rem 4rem;
+    }
+</style>
