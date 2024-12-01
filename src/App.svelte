fe79f02f27a2454a4fc40321736acd496fc9ac35
Noah Lehmann <noah.lehmann@protonmail.com>
2024-12-01 22:44:30 +0100

    main: Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,10 @@
 <script>
-  import { _ } from "svelte-i18n";
   import Header from "./components/header/Header.svelte";
+  import Footer from "./components/footer/Footer.svelte";
 </script>
 
 <main class="container">
   <Header />
-  <p>{$_('description')}</p>
-  <p>See how its deployed:</p>
-  <p>Intellij -> Github -> Actions -> Build -> Upload -> Serve -> Domain</p>
-  <a href="https://github.com/noahlehmann/portfolio/actions/workflows/static.yml">
-    <img alt="Deploy status badge"  src="https://github.com/noahlehmann/portfolio/actions/workflows/static.yml/badge.svg">
-  </a>
+
+  <Footer />
 </main>
