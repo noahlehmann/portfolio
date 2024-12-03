675745a9a8606201b7ece6e25c9de3532e9b5467
Noah Lehmann <noah.lehmann@protonmail.com>
2024-12-03 21:35:54 +0100

    main: refactor header

---
+++ src/components/header/MenuButton.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let text;
+</script>
+
+<button class="menu-button">
+    <p>{text}</p>
+</button>
+
+<style>
+    button {
+        all: unset;
+        cursor: pointer;
+    }
+    p {
+        margin: 0;
+    }
+    .menu-button {
+        border: 1px solid var(--color);
+        border-radius: 0.5rem;
+        font-size: 1.5rem;
+        padding: 0.5rem;
+    }
+</style>
